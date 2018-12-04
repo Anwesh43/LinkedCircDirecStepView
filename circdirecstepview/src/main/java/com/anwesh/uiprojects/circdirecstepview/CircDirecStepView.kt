df43cc0d2d723b43658e88571fab0e0a2f6909c1
Anwesh43 <anweshthecool0@gmail.com>
2018-12-04 11:36:36 +0530

    using renderer in view

--- circdirecstepview/src/main/java/com/anwesh/uiprojects/circdirecstepview/CircDirecStepView.kt
+++ circdirecstepview/src/main/java/com/anwesh/uiprojects/circdirecstepview/CircDirecStepView.kt
@@ -50,14 +50,16 @@ class CircDirecStepView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -200,7 +202,7 @@ class CircDirecStepView(ctx : Context) : View(ctx) {
         fun create(activity: Activity) : CircDirecStepView {
             val view : CircDirecStepView = CircDirecStepView(activity)
             activity.setContentView(view)
-            return view 
+            return view
         }
     }
 }
