e9f8d3e531872357b27021b047d34bd4ea036cf5
Anwesh43 <anweshthecool0@gmail.com>
2018-12-04 11:30:45 +0530

    adding CDSNode class

--- circdirecstepview/src/main/java/com/anwesh/uiprojects/circdirecstepview/CircDirecStepView.kt
+++ circdirecstepview/src/main/java/com/anwesh/uiprojects/circdirecstepview/CircDirecStepView.kt
@@ -109,4 +109,47 @@ class CircDirecStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CDSNode(var i : Int, val state : State = State()) {
+        private var prev : CDSNode? = null
+        private var next : CDSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = CDSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCDSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CDSNode {
+            var curr : CDSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
