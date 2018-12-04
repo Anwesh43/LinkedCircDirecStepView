695947e6e75855c947d80f351920a483ff110e8d
Anwesh43 <anweshthecool0@gmail.com>
2018-12-04 10:57:19 +0530

    adding libraries and defining constants

--- circdirecstepview/src/main/java/com/anwesh/uiprojects/circdirecstepview/CircDirecStepView.kt
+++ circdirecstepview/src/main/java/com/anwesh/uiprojects/circdirecstepview/CircDirecStepView.kt
@@ -3,3 +3,18 @@ package com.anwesh.uiprojects.circdirecstepview
 /**
  * Created by anweshmishra on 04/12/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.content.Context
+
+val nodes : Int = 5
+val sizeFactor : Float = 2.4f
+val strokeFactor : Int = 60
+val scDiv : Double = 0.51
+val scGap : Float = 0.05f
