c6e5ac16b928e9e0382de87363acd9da88e9b762
Anwesh43 <anweshthecool0@gmail.com>
2018-12-04 11:20:06 +0530

    adding drawCDSNode canvas extension method

--- circdirecstepview/src/main/java/com/anwesh/uiprojects/circdirecstepview/CircDirecStepView.kt
+++ circdirecstepview/src/main/java/com/anwesh/uiprojects/circdirecstepview/CircDirecStepView.kt
@@ -26,3 +26,22 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.getInverse(), Math.
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 
 fun Float.updateScale(dir : Float) = dir * scGap
+
+fun Canvas.drawCDSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sf : Float = 1f - 2 * (i % 2)
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    save()
+    translate(gap * (i + 1) + (w/2) * sc2 * sf, h/2)
+    rotate(90f * sc1 * sf)
+    drawArc(RectF(-size, -size, size, size), 60f, 300f, true, paint)
+    val x : Float = 1.5f * size * Math.cos(Math.PI/3).toFloat()
+    val y : Float = 1.5f * size * Math.sin(Math.PI/3).toFloat()
+    drawLine(0f, 0f, x, y, paint)
+    drawLine(0f, 0f, -x, y, paint)
+    restore()
+}
